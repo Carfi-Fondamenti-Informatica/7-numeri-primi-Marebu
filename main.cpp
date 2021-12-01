3086dff240fcb9b79f728b33ed2c935f3cf58f7b
Marebu <91844335+Marebu@users.noreply.github.com>
2021-12-01 21:49:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
-
 int main(){
-    int a=0;
+    int a=0, b=2;
     cin >> a;
-    bool ris= numeriprimi(a);
+    bool ris= numeriprimi(a,b);
     if(ris){
         cout << "numero primo" << endl;
     }else{
